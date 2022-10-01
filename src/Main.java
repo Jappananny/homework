c7bc2656d0c9bc668894d257d4478a17968fcb93
Jappananny <Jappananny@gmail.com>
2022-10-01 11:14:28 +0300

    задание 7

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         var boxer2Weight = 82.7;
         System.out.println("Общий вес двух бойцов: " + (boxer1Weight + boxer2Weight));
         System.out.println("Разница весов двух бойцов: " + (boxer2Weight - boxer1Weight));
-
+        //Задание 7
+        System.out.println("Задание 7");
+        System.out.println("Разница весов двух бойцов (1 способ):" + (boxer2Weight - boxer1Weight));
+        System.out.println("Разница весов двух бойцов (2 способ): " + (boxer2Weight % boxer1Weight));
 
 }
