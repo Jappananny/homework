a5361111cf4947e0061ae613f485a2c572c3c6e2
Jappananny <Jappananny@gmail.com>
2022-10-01 11:06:02 +0300

    задание 6

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 6
+        System.out.println("Задание 6");
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        System.out.println("Общий вес двух бойцов: " + (boxer1Weight + boxer2Weight));
+        System.out.println("Разница весов двух бойцов: " + (boxer2Weight - boxer1Weight));
+
+
+}
